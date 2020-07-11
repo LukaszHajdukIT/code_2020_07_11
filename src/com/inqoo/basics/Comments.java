d50bfb83b9e036d836dd81ea91c9dc8dd14e4331
LukaszHajdukIT <onevisage@interia.pl>
2020-07-11 12:36:27 +0200

    |Dodano komentarz, dodano sout

--- src/com/inqoo/basics/Comments.java
+++ src/com/inqoo/basics/Comments.java
@@ -5,11 +5,11 @@ public class Comments {
     public static void main(String[] args) {
         System.out.println("Witaj na "); //Powitanie
         System.out.println("kursie programowania");
-
+        // jakis komentarz
         /* Na koniec emotka,
           bo powaga zabija ;)
          */
         // ponizsza linijka wypisze emotikone
-        System.out.println(":)");
+
     }
 }
